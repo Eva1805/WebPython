cc2b9da7d6915f962c75dec640a625b0e1b2773c
Анна Ботт <bott_av@itsirius.su>
2018-06-28 13:26:14 +0300

    Изменения в html недели 5

--- week5/submissions/post_url.py
+++ week5/submissions/post_url.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 import requests
-import urllib.parse
 from requests.auth import HTTPBasicAuth
 import json
 
 url = 'http://79.137.175.13/submissions/1/'
-login='alladin'
+login = 'alladin'
 passw = 'opensesame'
 res = requests.post(url,  auth=HTTPBasicAuth(login, passw))
 r = res.json()
 print(r)
 
 url = 'http://79.137.175.13/submissions/super/duper/secret/'
-login='galchonok'
+login = 'galchonok'
 passw = 'ktotama'
 res = requests.put(url,  auth=HTTPBasicAuth(login, passw))
 r = res.json()
 print(r)
 with open('answer.json', 'w') as f:
     json.dump(r, f)
+with open('answer.json', 'r') as f:
+    print(json.load(f))
